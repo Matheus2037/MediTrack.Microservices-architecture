a0da39020320a126bc1194413a64ddbbd6d0406b
Vinicius Milanez <viniciussmilanez@hotmail.com>
2024-12-05 22:55:14 -0300

    .

--- MediTrack.Consulta/Consulta/Program.cs
+++ MediTrack.Consulta/Consulta/Program.cs
@@ -7,6 +7,7 @@ using Consulta.DataBase.EF;
 using Microsoft.EntityFrameworkCore;
 using Consulta.Servicos;
 using Consulta.Domain.Consulta;
+using Hangfire.Storage.SQLite;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,23 +20,26 @@ builder.Services.AddSwaggerGen();
 var mongoUrlBuilder = new MongoUrlBuilder("mongodb://localhost/admin");
 var mongoClient = new MongoClient(mongoUrlBuilder.ToMongoUrl());
 
-// Add Hangfire services. Hangfire.AspNetCore nuget required
+//// Add Hangfire services. Hangfire.AspNetCore nuget required
+//builder.Services.AddHangfire(configuration => configuration
+//    .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+//    .UseSimpleAssemblyNameTypeSerializer()
+//    .UseRecommendedSerializerSettings()
+//    .UseMongoStorage(mongoClient, mongoUrlBuilder.DatabaseName, new MongoStorageOptions
+//    {
+//        MigrationOptions = new MongoMigrationOptions
+//        {
+//            MigrationStrategy = new MigrateMongoMigrationStrategy(),
+//            BackupStrategy = new CollectionMongoBackupStrategy()
+//        },
+//        Prefix = "hangfire.mongo",
+//        CheckConnection = true
+//    })
+//);
 builder.Services.AddHangfire(configuration => configuration
-    .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
-    .UseSimpleAssemblyNameTypeSerializer()
-    .UseRecommendedSerializerSettings()
-    .UseMongoStorage(mongoClient, mongoUrlBuilder.DatabaseName, new MongoStorageOptions
-    {
-        MigrationOptions = new MongoMigrationOptions
-        {
-            MigrationStrategy = new MigrateMongoMigrationStrategy(),
-            BackupStrategy = new CollectionMongoBackupStrategy()
-        },
-        Prefix = "hangfire.mongo",
-        CheckConnection = true
-    })
-);
-
+            .UseSimpleAssemblyNameTypeSerializer()
+            .UseRecommendedSerializerSettings()
+            .UseSQLiteStorage());
 
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlite("Data Source=consultas.db"));
@@ -43,7 +47,7 @@ builder.Services.AddDbContext<DataContext>(options =>
 // Add the processing server as IHostedService
 builder.Services.AddHangfireServer(serverOptions =>
 {
-    serverOptions.ServerName = "Hangfire.Mongo server 1";
+    serverOptions.ServerName = "Hangfire server 1";
 });
 
 builder.Services.AddScoped<IServiceConulta, ServConsulta>();
